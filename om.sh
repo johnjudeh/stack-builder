2dbddfbcce0543ac5e546504243a8f39dbeb7368
John Judeh <johnjudeh@hotmail.com>
2020-03-02 19:17:51 +0000

    style: Restructure the layout of function and create new titles

--- om.sh
+++ om.sh
@@ -107,7 +107,7 @@ readonly projects=( \
 verbose_mode='false'
 
 
-#### FUNCTIONS ####
+#### MAPPING FUNCTIONS ####
 
 function get_dir() {
 	local project="$1"
@@ -172,6 +172,9 @@ function get_code_env_name() {
 	return 0
 }
 
+
+#### UTILITY FUNCTIONS ####
+
 function is_in_array() {
 	local search="$1"
 	local arr=("${@:2}")
@@ -205,6 +208,76 @@ function is_valid_run_command_option() {
 	is_in_array "$search" "${[@]}"
 
 }
+
+function print_format() {
+	local type="$1"
+	local msg="$2"
+
+	case "$type" in
+		"$style_command_title")
+			printf "${fm_yellow}==>${fm_reset} $2\n"
+			printf "%s\n" "------------------------------------------------------------" 
+			;;
+		"$style_error")
+			printf "ERR! $2\n"
+			;;
+	esac
+
+	return 0
+}
+
+
+#### BASE TASK FUNCTIONS ###
+
+function activate_code_env() {
+	local code_type="$1"
+	local env_name="$2"
+	local run_install="$3"
+	local title="Loading $code_type environment '$env_name'$([[ "$run_install" != 'true' ]] ||  printf ' (with package install)')"
+
+	print_format "$style_command_title" "$title"
+
+	#TODO: Figure out how to export this to the shell that called it
+	source "$VENV_ROOT/$code_type/$env_name/bin/activate" || return 1
+
+	if [[ "$run_install" = 'true'  ]]; then
+		case "$code_type" in
+			"$code_type_py")
+				pip install -r requirements.txt || return 1
+				;;
+			"$code_type_node")
+				npm install || return 1
+				git checkout -- package-lock.json || return 1
+				;;
+		esac
+	fi
+
+	return 0
+}
+
+function change_dir() {
+	local dir_name="$1"
+	print_format "$style_command_title" "Changing directory to '$dir_name'"
+	cd "$dir_name"
+}
+
+function checkout_git_branch() {
+	local branch_name="$1"
+	print_format "$style_command_title" "Checking out branch '$branch_name'"
+	git fetch --all
+	git checkout "$branch_name"
+	git pull
+}
+
+function run_command() {
+	print_format "$style_command_title" "Running command '$*'"
+	$@
+}
+
+
+
+
+#### COMMAND-SPECIFIC FUNCTIONS ####
 
 function handle_base_options() {
 	local option="$1"
@@ -265,68 +338,6 @@ function check_env() {
 	fi
 
 	return 0
-}
-
-function print_format() {
-	local type="$1"
-	local msg="$2"
-
-	case "$type" in
-		"$style_command_title")
-			printf "${fm_yellow}==>${fm_reset} $2\n"
-			printf "%s\n" "------------------------------------------------------------" 
-			;;
-		"$style_error")
-			printf "ERR! $2\n"
-			;;
-	esac
-
-	return 0
-}
-
-function activate_code_env() {
-	local code_type="$1"
-	local env_name="$2"
-	local run_install="$3"
-	local title="Loading $code_type environment '$env_name'$([[ "$run_install" != 'true' ]] ||  printf ' (with package install)')"
-
-	print_format "$style_command_title" "$title"
-
-	#TODO: Figure out how to export this to the shell that called it
-	source "$VENV_ROOT/$code_type/$env_name/bin/activate" || return 1
-
-	if [[ "$run_install" = 'true'  ]]; then
-		case "$code_type" in
-			"$code_type_py")
-				pip install -r requirements.txt || return 1
-				;;
-			"$code_type_node")
-				npm install || return 1
-				git checkout -- package-lock.json || return 1
-				;;
-		esac
-	fi
-
-	return 0
-}
-
-function change_dir() {
-	local dir_name="$1"
-	print_format "$style_command_title" "Changing directory to '$dir_name'"
-	cd "$dir_name"
-}
-
-function checkout_git_branch() {
-	local branch_name="$1"
-	print_format "$style_command_title" "Checking out branch '$branch_name'"
-	git fetch --all
-	git checkout "$branch_name"
-	git pull
-}
-
-function run_command() {
-	print_format "$style_command_title" "Running command '$*'"
-	$@
 }
 
 function run() {
